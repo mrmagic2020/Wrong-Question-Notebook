7270bd5aa6d4a2dc10da00f0d1b72500ca0f872d
Arnold Zhou <magicarnold2022@gmail.com>
2026-02-22 11:03:51 +1100

    fix(#33): error in password reset flow
    * fix: error in password reset flow
    
    * fix: keep fixing

--- web/app/auth/confirm/route.ts
+++ web/app/auth/confirm/route.ts
@@ -1,13 +1,15 @@
 import { createClient } from '@/lib/supabase/server';
 import { type EmailOtpType } from '@supabase/supabase-js';
 import { redirect } from 'next/navigation';
-import { type NextRequest } from 'next/server';
+import { NextResponse, type NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const token_hash = searchParams.get('token_hash');
   const type = searchParams.get('type') as EmailOtpType | null;
   const next = searchParams.get('next') ?? '/';
+  const redirectTo = request.nextUrl.clone();
+  redirectTo.pathname = next;
 
   if (token_hash && type) {
     const supabase = await createClient();
@@ -18,7 +20,7 @@ export async function GET(request: NextRequest) {
     });
     if (!error) {
       // redirect user to specified redirect URL or root of app
-      redirect(next);
+      return NextResponse.redirect(redirectTo);
     } else {
       // redirect the user to an error page with some instructions
       redirect(`/auth/error?error=${error?.message}`);
@@ -26,5 +28,6 @@ export async function GET(request: NextRequest) {
   }
 
   // redirect the user to an error page with some instructions
-  redirect(`/auth/error?error=No token hash or type`);
+  redirectTo.pathname = '/auth/error';
+  return NextResponse.redirect(redirectTo);
 }
